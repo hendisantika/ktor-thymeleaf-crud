c83646cd4416aabb928fe2d3db7ea4f0b22c4403
hendisantika <hendisantika@yahoo.co.id>
2025-08-01 06:16:56 +0700

    feat: Add required libs🫸🌀✏️📗🐧🐳

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+val kotlin_version: String by project
+val logback_version: String by project
+val exposed_version: String by project
+val postgres_version: String by project
+val hikaricp_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.1.10"
+    id("io.ktor.plugin") version "3.2.2"
+}
+
+group = "com.example"
+version = "0.0.1"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Ktor core
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("io.ktor:ktor-server-config-yaml")
+
+    // Thymeleaf
+    implementation("io.ktor:ktor-server-thymeleaf")
+
+    // Database
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
+    implementation("org.postgresql:postgresql:$postgres_version")
+    implementation("com.zaxxer:HikariCP:$hikaricp_version")
+
+    // Logging
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    // Testing
+    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
