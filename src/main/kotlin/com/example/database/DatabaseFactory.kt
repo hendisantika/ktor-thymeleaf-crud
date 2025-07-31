b7de921f0a89d82afc8805fd60d64f2f361af578
hendisantika <hendisantika@yahoo.co.id>
2025-08-01 06:17:17 +0700

    feat: Add DatabaseFactory🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/com/example/database/DatabaseFactory.kt
@@ -0,0 +1,41 @@
+package com.example.database
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import io.ktor.server.config.*
+import kotlinx.coroutines.Dispatchers
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object DatabaseFactory {
+    fun init(config: ApplicationConfig) {
+        val driverClassName = config.property("database.driverClassName").getString()
+        val jdbcURL = config.property("database.jdbcURL").getString()
+        val username = config.property("database.username").getString()
+        val password = config.property("database.password").getString()
+        val maximumPoolSize = config.property("database.maximumPoolSize").getString().toInt()
+
+        val dataSource = HikariDataSource(HikariConfig().apply {
+            this.driverClassName = driverClassName
+            this.jdbcUrl = jdbcURL
+            this.username = username
+            this.password = password
+            this.maximumPoolSize = maximumPoolSize
+            this.isAutoCommit = false
+            this.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+            this.validate()
+        })
+
+        Database.connect(dataSource)
+
+        // Initialize database tables
+        transaction {
+            SchemaUtils.create(Products)
+        }
+    }
+
+    suspend fun <T> dbQuery(block: suspend () -> T): T =
+        newSuspendedTransaction(Dispatchers.IO) { block() }
+}
